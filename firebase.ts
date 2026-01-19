36be68e5de813746094ff48d0b387cfe79bcb302
Sreekanth K <kanth5994@gmail.com>
2026-01-19 15:15:41 +0530

    Security Issue Fixed

--- firebase.ts
+++ firebase.ts
@@ -1,20 +1,20 @@
-
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD92qeOqo1v5NkgPvv5_Cn1o-SeRdunY24",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "parkspace-share-pro.firebaseapp.com",
   projectId: "parkspace-share-pro",
-  storageBucket: "parkspace-share-pro.firebasestorage.app",
+  storageBucket: "parkspace-share-pro.appspot.com",
   messagingSenderId: "158847640433",
   appId: "1:158847640433:web:d2dde5985daded10694cd0",
-  measurementId: "G-KXK2EHHW9L"
+  measurementId: "G-KXK2EHHW9L",
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
